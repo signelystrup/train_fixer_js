3f4928d907459d8786cc46026154687e9ad2acf4
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-09-13 19:14:21 +0200

    added logic for locomotives in isValid()

--- train.js
+++ train.js
@@ -12,16 +12,47 @@ class Train{
 
     isValid(){
 
-        if (this.trainList.head.data.type !== "lokomotiv"){
-            return false;
+        if (this.trainList.isEmpty){
+            return true; //? it's not NOT valid.
         }
 
         const size = this.trainList.size();
 
-        if (size > 10 && this.trainList.tail.data.type !== "lokomotiv"){
+        //LOKOMOTIV
+        if (this.trainList.head.data !== lokomotiv){ //the first car must always be a locomotive.
             return false;
         }
 
+        if (size <= 10 && this.trainList.tail.data === lokomotiv){
+            return false;
+        }
+
+        if (size > 10 && this.trainList.tail.data !== lokomotiv){ //if there are more than 10 cars, the last car must be a locomotive.
+            return false;
+        }
+
+        let i = 0;
+        //check for locomotives in the middle of the train.
+        this.trainList.forEach(car => {
+            if (car === lokomotiv &&
+                i !== 0 &&
+                i !== size-1){
+
+                return false;
+            }
+
+            i++;
+        });
+
+        //PASSAGERVOGNE
+
+        //SIDDEVOGNE
+        //SENGEVOGNE
+        //SPISEVOGNE
+
+
+
+        //GODSVOGNE
 
 
         return true;
@@ -32,6 +63,12 @@ class Train{
     }
 
     printTrain(){
+        if (this.trainList.isEmpty()){
+            console.log("train is empty");
+        }else {
+            console.log("Is train valid? " + this.isValid() );
+        }
+
         this.trainList.forEach(car => console.log("car: " + car.type + ", " + car.subType));
     }
 
