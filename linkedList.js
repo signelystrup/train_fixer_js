7b839af5a8c46d60faf16999f0a18509dab0c8eb
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-09-13 18:26:01 +0200

    remove() and size() functions

--- linkedList.js
+++ linkedList.js
@@ -11,7 +11,7 @@ class LinkedList {
     add(data) {
         let newNode = {data: data, next: null};
 
-        if (this.head == null){ // list is empty
+        if (this.head === null){ // list is empty
             this.head = newNode;
             this.tail = newNode;
         }else{
@@ -21,14 +21,57 @@ class LinkedList {
     }
 
     remove(data) {
+        if (data === null){
+            return false;
+        }
 
+        //if list empty:
+        if (this.head === null){
+            return false;
+        }
+
+        //if target is first element in list:
+        if (this.head.data === data){
+            this.head = this.head.next;
+
+            return true;
+        }
+
+
+        let current = this.head.next;
+        let previous = this.head;
+
+        while (current !== null) {
+            if (current.data === data) {
+                previous.next = current.next;
+                return true;
+
+            }
+
+            //next index:
+            previous = current;
+            current = current.next;
+
+        }//end of while loop.
     }
 
     size() {
+        let current = this.head;
+        let counter = 0;
 
+        while (current !== null){
+            counter ++;
+            current = current.next;
+        }
+
+        return counter;
     }
 
     get(index) {
+
+    }
+
+    indexOf(data){
 
     }
 
@@ -36,7 +79,7 @@ class LinkedList {
 
         let current = this.head;
 
-        while (current != null){
+        while (current !== null){
 
             callBack(current.data);
             current = current.next; //go to next index
@@ -44,7 +87,7 @@ class LinkedList {
     }
 
     isEmpty(){
-        if (this.head == null){
+        if (this.head === null){
             return true;
         }else{
             return false;
